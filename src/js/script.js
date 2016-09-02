9ab038463393b16cd2534182234531ba82236f81
fujisawa-shin <fujisawa-shin@kayac.com>
2016-09-02 09:37:23 +0900

    three.jsのデモ

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,31 @@
-import Sample from './lib/Sample';
-import $ from 'jquery';
+const THREE = require("three");
 
-const sample = new Sample({
-    name: 'world'
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+camera.position.z = 1000;
+
+const geometry = new THREE.BoxGeometry(200, 200, 200);
+const material = new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+    wireframe: true
 });
 
-$('.wrapper').on('click', () => {
-    console.log(`hello, ${sample.name}.`);
-});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+
+(function animate() {
+    requestAnimationFrame(animate);
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+
+    renderer.render(scene, camera);
+})();
